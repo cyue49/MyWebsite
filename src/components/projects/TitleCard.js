9f25398845e7cb131732581938ef21a67d15226b
Chen Yue <yuechen049@gmail.com>
2024-08-22 18:09:42 -0400

    updated projects section with technologies and description and some styling

--- src/components/projects/TitleCard.js
+++ src/components/projects/TitleCard.js
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react'
 import { LanguageContext } from '../../App'
+import BubbledList from '../common/BubbledList'
 
 const TitleCard = ({ project }) => {
     const language = useContext(LanguageContext);
 
     return (
-        <div className='size-fit flex flex-col items-center justify-center rounded-3xl border border-lightColor p-4'>
+        <div className='w-full lg:w-[48%] flex flex-col items-start justify-start rounded-3xl border border-lightColor/40 hover:border-lightColor hover:scale-[1.02] p-4 cursor-pointer hover-animation'>
             <div className='font-bold'>{(language === 'en') ? project.en.title : project.fr.title}</div>
+            <BubbledList list={project.technologies}/>
+            <div>{(language === 'en') ? project.en.description : project.fr.description}</div>
         </div>
     )
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,7 +5,7 @@ import TitleCard from '../components/projects/TitleCard'
 const Projects = () => {
     return (
         <div className='px-4 lg:px-6 py-3'>
-            <div className='flex flex-row gap-4 flex-wrap'>
+            <div className='flex flex-row gap-6 py-4 flex-wrap items-stretch justify-start lg:justify-between'>
                 {projects.map((project, index) => (
                     <TitleCard project={project} index={index} />
                 ))}
