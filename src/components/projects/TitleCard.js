aa1f8541f2e08d3c3718a75ddd9647fca718df57
Chen Yue <yuechen049@gmail.com>
2024-08-22 18:34:07 -0400

    updated project links

--- src/components/projects/TitleCard.js
+++ src/components/projects/TitleCard.js
@@ -1,14 +1,30 @@
 import React, { useContext } from 'react'
 import { LanguageContext } from '../../App'
 import BubbledList from '../common/BubbledList'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
 
 const TitleCard = ({ project }) => {
     const language = useContext(LanguageContext);
 
     return (
-        <div className='w-full lg:w-[48%] flex flex-col items-start justify-start rounded-3xl border border-lightColor/40 hover:border-lightColor hover:scale-[1.02] p-4 cursor-pointer hover-animation'>
+        <div className='w-full lg:w-[48%] flex flex-col gap-2 items-start justify-start rounded-3xl border border-lightColor/40 hover:border-lightColor hover:scale-[1.02] p-4 cursor-pointer hover-animation'>
             <div className='font-bold'>{(language === 'en') ? project.en.title : project.fr.title}</div>
-            <BubbledList list={project.technologies}/>
+
+            <BubbledList list={project.technologies} />
+
+            <div className='flex flex-row items-center justify-start gap-5'>
+                {(project.links.length === 0) ? null : project.links.map((link, index) => (
+                    <a href={link.link} target='_blank' rel='noreferrer'
+                        className='flex flex-row gap-2 items-center border-b-2 border-b-transparent hover:border-primaryColor shadow-type-3 hover:scale-105 hover-animation'>
+                        <FontAwesomeIcon icon={(link.name === 'GitHub') ? faGithub : faLink} className='p-0 m-0' />
+                        {link.name}
+                    </a>
+                ))}
+            </div>
+
+
             <div>{(language === 'en') ? project.en.description : project.fr.description}</div>
         </div>
     )
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,10 +7,9 @@ const Projects = () => {
         <div className='px-4 lg:px-6 py-3'>
             <div className='flex flex-row gap-6 py-4 flex-wrap items-stretch justify-start lg:justify-between'>
                 {projects.map((project, index) => (
-                    <TitleCard project={project} index={index} />
+                    <TitleCard project={project} key={index} />
                 ))}
             </div>
-
         </div>
     )
 }
