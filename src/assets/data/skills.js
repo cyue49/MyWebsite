04fa88510de13e2952825c474019ac43c347db16
Chen Yue <yuechen049@gmail.com>
2024-09-19 21:21:31 -0400

    updates

--- src/assets/data/skills.js
+++ src/assets/data/skills.js
@@ -5,23 +5,23 @@ export const skills = [
         list: {
             en: [
                 'Able to work in Windows, MacOs, and Linux environments.',
+                'Create, use, and maintain databases in applications.',
+                'Perform testing and debugging of code.',
+                'Create and use APIs.',
                 'Full-stack web development working with both frontend and backend technologies.',
                 'Native application development in IOS using Swift and SwiftUI.',
                 'Native application development in Android using Kotlin or Java and Jetpack Compose or XML.',
-                'Multiplatform application development using Kotlin Multiplatform and Compose Multiplatform.',
-                'Create, use, and maintain databases in applications.',
-                'Perform testing and debugging of code.',
-                'Create and use APIs.'
+                'Multiplatform application development using React Native with Expo or using Kotlin Multiplatform and Compose Multiplatform.'
             ],
             fr: [
                 'Habileté de travailler dans un environnement Windows, MacOs et Linux.',
+                'Créer, utiliser et maintenir des bases de données dans les applications.',
+                'Effectuer des tests et le débogage des codes.',
+                'Créer et utiliser des APIs.',
                 'Développement web full-stack en utilisant des technologies frontend et backend.',
                 'Développement d\'applications natives avec IOS en utilisant Swift et SwiftUI.',
                 'Développement d\'applications natives avec Android en utilisant Kotlin ou Java et Jetpack Compose ou XML.',
-                'Développement d\'applications multiplateformes en utilisant Kotlin Multiplateforme et Compose Multiplateforme.',
-                'Créer, utiliser et maintenir des bases de données dans les applications.',
-                'Effectuer des tests et le débogage des codes.',
-                'Créer et utiliser des APIs.'
+                'Développement d\'applications multiplateformes en utilisant React Native avec Expo ou en utilisant Kotlin Multiplateforme et Compose Multiplateforme.'
             ]
         }
     },
--- src/assets/data/technologies.js
+++ src/assets/data/technologies.js
@@ -2,12 +2,17 @@ export const technologies = [
     {
         en: "Programming languages",
         fr: "Langues de programmation",
-        list: ['Java', 'Python', 'C', 'C++', 'Swift', 'Kotlin']
+        list: ['Java', 'Python', 'C', 'C++', 'Kotlin']
     },
     {
         en: "Web Development",
         fr: "Développement web",
         list: ['HTML', 'CSS', 'JavaScript', 'PHP', 'JQuery', 'React', 'Node.js', 'Bootstrap', 'Tailwind CSS']
+    },
+    {
+        en: "Mobile Development",
+        fr: "Développement mobile",
+        list: ['React Native', 'Expo', 'Swift', 'SwiftUI', 'Kotlin Multiplatform']
     },
     {
         en: "Database",
@@ -17,7 +22,7 @@ export const technologies = [
     {
         en: "Development Tools",
         fr: "Outils de développement",
-        list: ['Intellij', 'Visual Studio Code', 'Eclipse', 'Xcode', 'Android Studio', 'GitHub']
+        list: ['GitHub', 'Intellij', 'Visual Studio Code', 'Eclipse', 'Xcode', 'Android Studio', 'Jira', 'Trello']
     },
     {
         en: "Other Tools",
